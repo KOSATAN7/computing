4610f5756a0cca39208dc03581d9f1192d4bc072
irsyaad06 <irsyadfatahilah@gmail.com>
2025-02-04 21:25:17 +0700

    refactor(seeder) : change email datas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,11 +11,11 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::insert([
-            ['username' => 'superadmin', 'email' => 'superadmin@example.com', 'password' => Hash::make('superadmin123'), 'role' => 'super_admin'],
-            ['username' => 'admin_venue_1', 'email' => 'adminvenue1@example.com', 'password' => Hash::make('admin123'), 'role' => 'admin_venue'],
-            ['username' => 'admin_venue_2', 'email' => 'adminvenue2@example.com', 'password' => Hash::make('admin123'), 'role' => 'admin_venue'],
-            ['username' => 'infobar_1', 'email' => 'infobar1@example.com', 'password' => Hash::make('infobar123'), 'role' => 'infobar'],
-            ['username' => 'infobar_2', 'email' => 'infobar2@example.com', 'password' => Hash::make('infobar123'), 'role' => 'infobar'],
+            ['username' => 'superadmin', 'email' => 'superadmin@kosatan.com', 'password' => Hash::make('superadmin123'), 'role' => 'super_admin'],
+            ['username' => 'admin_venue_1', 'email' => 'adminvenue1@kosatan.com', 'password' => Hash::make('admin123'), 'role' => 'admin_venue'],
+            ['username' => 'admin_venue_2', 'email' => 'adminvenue2@kosatan.com', 'password' => Hash::make('admin123'), 'role' => 'admin_venue'],
+            ['username' => 'infobar_1', 'email' => 'infobar1@kosatan.com', 'password' => Hash::make('infobar123'), 'role' => 'infobar'],
+            ['username' => 'infobar_2', 'email' => 'infobar2@kosatan.com', 'password' => Hash::make('infobar123'), 'role' => 'infobar'],
         ]);
     }
 }
